c3c530d8c6d5d1e0917fbe571b699831b2768734
PameGiribone <pame.giribone@hotmail.com>
2024-11-26 23:31:19 -0300

    ap en local

--- mundoAnimal/src/main/java/com/backend/mundoAnimal/security/WebConfig.java
+++ mundoAnimal/src/main/java/com/backend/mundoAnimal/security/WebConfig.java
@@ -3,21 +3,22 @@ package com.backend.mundoAnimal.security;
 import java.util.Arrays;
 import java.util.List;
 
-import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration 
-public class WebConfig implements  WebMvcConfigurer{
-      @Override 
-      // Método para configurar los convertidores de mensajes 
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    // Método para configurar los convertidores de mensajes 
     public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
         MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
         converter.setSupportedMediaTypes(Arrays.asList(MediaType.APPLICATION_JSON, MediaType.APPLICATION_JSON_UTF8));
         converters.add(converter);
     }
 
-
+    
 }
